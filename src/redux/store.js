9bc93f85551858d22419d10220669bd143259239
olga-plotkina <olga.semenyuta@gmail.com>
2022-12-07 23:19:36 +0000

    fix persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,30 +1,24 @@
-// import { configureStore } from '@reduxjs/toolkit';
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-const persistConfig = {
-  key: 'contacts',
-  storage,
-};
-
-// export let store = configureStore({
-//   reducer: {
-//     contacts: contactsReducer,
-//     filter: filterReducer,
-//   },
-//   persistConfig,
-// });
-
 export const rootReducer = combineReducers({
   contacts: contactsReducer,
   filter: filterReducer,
 });
 
+const persistConfig = {
+  key: 'contacts',
+  storage,
+  whitelist: ['contacts'],
+};
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-export let store = createStore(persistedReducer);
+export let store = configureStore({
+  reducer: persistedReducer,
+});
 
 export let persistor = persistStore(store);
