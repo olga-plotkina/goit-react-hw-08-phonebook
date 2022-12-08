867923571d4a6ebf4b123e40fdf44e2d362b2363
olga-plotkina <olga.semenyuta@gmail.com>
2022-12-08 22:31:36 +0000

    add another addMatcher

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -14,18 +14,12 @@ const contactsSlice = createSlice({
   extraReducers: builder =>
     builder
       .addCase(fetchContacts.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.error = null;
         state.items = action.payload;
       })
       .addCase(addContact.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.error = null;
         state.items.push(action.payload);
       })
       .addCase(deleteContact.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.error = null;
         const index = state.items.findIndex(
           contact => contact.id === action.payload.id
         );
@@ -51,33 +45,18 @@ const contactsSlice = createSlice({
           state.isLoading = false;
           state.error = action.payload;
         }
+      )
+      .addMatcher(
+        isAnyOf(
+          fetchContacts.fulfilled,
+          addContact.fulfilled,
+          deleteContact.fulfilled
+        ),
+        state => {
+          state.isLoading = false;
+          state.error = null;
+        }
       ),
-  // extraReducers: {
-  //   [fetchContacts.pending]: handlePending,
-  //   [addContact.pending]: handlePending,
-  //   [deleteContact.pending]: handlePending,
-  //   [fetchContacts.rejected]: handleRejected,
-  //   [addContact.rejected]: handleRejected,
-  //   [deleteContact.rejected]: handleRejected,
-  //   [fetchContacts.fulfilled](state, action) {
-  //     state.isLoading = false;
-  //     state.error = null;
-  //     state.items = action.payload;
-  //   },
-  //   [addContact.fulfilled](state, action) {
-  //     state.isLoading = false;
-  //     state.error = null;
-  //     state.items.push(action.payload);
-  //   },
-  //   [deleteContact.fulfilled](state, action) {
-  //     state.isLoading = false;
-  //     state.error = null;
-  //     const index = state.items.findIndex(
-  //       contact => contact.id === action.payload.id
-  //     );
-  //     state.items.splice(index, 1);
-  //   },
-  // },
 });
 
 export const contactsReducer = contactsSlice.reducer;
