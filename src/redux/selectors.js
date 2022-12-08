c335bd22e09692f01b0afb37e0627460954774ad
olga-plotkina <olga.semenyuta@gmail.com>
2022-12-08 21:22:09 +0000

    add createSelector

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,13 +1,15 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContacts = state => state.contacts.items;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 export const selectFilterString = state => state.filter;
 
-export const selectVisibleContacts = state => {
-  const contacts = selectContacts(state);
-  const filterString = selectFilterString(state);
-
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filterString.toLowerCase())
-  );
-};
+export const selectVisibleContacts = createSelector(
+  [selectContacts, selectFilterString],
+  (contacts, filterString) => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filterString.toLowerCase())
+    );
+  }
+);
