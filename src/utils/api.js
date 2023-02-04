2f78cdb26f876e142d63453e610db7f1c6a623c9
olga-plotkina <olga.semenyuta@gmail.com>
2023-02-04 15:34:45 +0000

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://connections-api.herokuapp.com',
+  baseURL: 'http://localhost:8084',
 });
 
 export const setAuthHeader = token => {
